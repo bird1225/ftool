52675482d33a2679faa423d52fef529af39e91a1
汪凌峰 <540914848@qq.com>
2024-10-14 23:54:32 +0800

    【初始化】项目结构初始化

--- app/core/config.py
+++ app/core/config.py
@@ -4,3 +4,11 @@
 @Date: 2024/10/13 23:46
 配置文件（例如数据库、环境变量）
 """
+
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+
+settings = Settings()
--- app/models/ftool_model.py
+++ app/models/ftool_model.py
@@ -4,3 +4,10 @@
 @Date: 2024/10/13 23:48
 工具相关的数据模型
 """
+from sqlalchemy import Column, Integer, String
+from app.db.base_class import Base
+
+class Tool(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
